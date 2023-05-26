41c456a93bcf62c94fb6a5a14227358fa2161015
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2023-05-26 15:19:30 +0700

    fix image not load

--- components/HomePage/product.vue
+++ components/HomePage/product.vue
@@ -29,7 +29,7 @@ const products = [
       <div class="grid grid-cols-2 gap-6 md:gap-8 mt-8">
          <div class="bg-white card-hover transition-all shadow-xl shadow-gray-100" v-for="product in products" :key="product">
             <div class="p-6 flex gap-8 items-center">
-               <img class="w-40 h-40 object-cover" :src="'/_nuxt/assets/images/' + product.image" alt="Product Gorong gorong" />
+               <img class="w-40 h-40 object-cover" :src="'/images/' + product.image" alt="Product Gorong gorong" />
                <div>
                   <h4 class="text-2xl font-bold mb-4">{{ product.title }}</h4>
                   <NuxtLink to="/products" class="text-red-500 text-lg font-bold group">Liat <i class="ai-chevron-right group-hover:pl-1 transition-all duration-300"></i></NuxtLink>
